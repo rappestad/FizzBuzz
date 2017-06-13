02db5f110fea7f02018d65e9e19b4c4e41a9ba53
Olof Rappestad <olof.rappestad@gmail.com>
2017-06-13 17:32:11 +0200

    red: should return [0, 1, 2] given (0, 2)

--- fizz-buzz.spec.js
+++ fizz-buzz.spec.js
@@ -9,4 +9,8 @@ describe('FizzBuzz', () => {
   it('should return [0, 1] given (0, 1)', () => {
     expect(fizzBuzz(0, 1)).to.deep.equal([0, 1])
   })
+
+  it('should return [0, 1, 2] given (0, 2)', () => {
+    expect(fizzBuzz(0, 2)).to.deep.equal([0, 1, 2])
+  })
 })
