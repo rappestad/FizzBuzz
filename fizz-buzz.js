e159f2a27137edf7912f18b3a48b755f374b3641
Olof Rappestad <olof.rappestad@gmail.com>
2017-06-13 17:45:50 +0200

    green: handle corner case (0) and Fizz (mod 3)

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,5 +1,15 @@
 function fizzBuzz(start, end) {
-  return Array.from({ length: end - start + 1 }, (v, k) => k + start)
+  return Array.from({ length: end - start + 1 }, (v, k) => {
+    if (k + start === 0) {
+      return 0
+    }
+
+    if ((k + start) % 3 === 0) {
+      return 'Fizz'
+    }
+
+    return k + start
+  })
 }
 
 module.exports = fizzBuzz
