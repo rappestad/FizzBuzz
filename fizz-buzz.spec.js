2609af2a2e032a3e4a5cf0aeea2d1d54cb8d3fa4
Olof Rappestad <olof.rappestad@gmail.com>
2017-06-13 18:18:44 +0200

    green: add acceptance test

--- fizz-buzz.spec.js
+++ fizz-buzz.spec.js
@@ -25,4 +25,14 @@ describe('FizzBuzz', () => {
   it('should return ["FizzBuzz", 16, 17] given (15, 17)', () => {
     expect(fizzBuzz(15, 17)).to.deep.equal(['FizzBuzz', 16, 17])
   })
+
+  it('should return correct answers given (1, 50)', () => {
+    expect(fizzBuzz(1, 50)).to.deep.equal([
+      1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz',
+      11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz',
+      'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz',
+      31, 32, 'Fizz', 34, 'Buzz', 'Fizz', 37, 38, 'Fizz', 'Buzz',
+      41, 'Fizz', 43, 44, 'FizzBuzz', 46, 47, 'Fizz', 49, 'Buzz'
+    ])
+  })
 })
